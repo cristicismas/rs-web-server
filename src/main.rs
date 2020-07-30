894f7bfac46797318bb67a725b5b33f939a4d3b9
Cristi Cismas <cristicismas99@gmail.com>
2020-07-30 09:41:47 +0300

    Only display the hello.html page on / route, and display a 404 on other routes

--- src/main.rs
+++ src/main.rs
@@ -12,29 +12,34 @@ fn main() {
     }
 }
 
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    read_and_print_connection(&mut stream, &mut buffer);
+
+    respond_with_html(&mut stream, buffer);
+
+}
+
 fn read_and_print_connection(stream: &mut TcpStream, buffer: &mut [u8; 1024]) {
     stream.read(buffer).unwrap();
     
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
 
-fn respond_with_html(stream: &mut TcpStream) {
-    let contents = fs::read_to_string("hello.html").unwrap();
+fn respond_with_html(stream: &mut TcpStream, buffer: [u8; 1024]) {
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-
-    read_and_print_connection(&mut stream, &mut buffer);
-
-    respond_with_html(&mut stream);
 }
