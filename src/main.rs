e8307f19dfbb9075ec4966522eaf201e7359e184
Cristi Cismas <cristicismas99@gmail.com>
2020-07-30 08:56:54 +0300

    Refactor the code into more functions and respond with an html page

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -11,15 +12,29 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-
-    stream.read(&mut buffer).unwrap();
+fn read_and_print_connection(stream: &mut TcpStream, buffer: &mut [u8; 1024]) {
+    stream.read(buffer).unwrap();
     
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+fn respond_with_html(stream: &mut TcpStream) {
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    read_and_print_connection(&mut stream, &mut buffer);
+
+    respond_with_html(&mut stream);
 }
